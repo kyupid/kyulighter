5700d2c488d6d6a12a3a59548241b88445a96d84
kyupid <iamscvv@gmail.com>
2021-10-24 22:38:32 +0900

    셀렉된 텍스트 위치 정보 가져오기

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-document.addEventListener("mouseup", (event) => {
-  if (window.getSelection().toString().length) {
-    let exactText = window.getSelection().toString();
-    console.log(exactText);
-  }
-});
+// document.addEventListener("mouseup", (event) => {
+//   if (window.getSelection().toString().length) {
+//     let exactText = window.getSelection().toString();
+//     console.log(exactText);
+//   }
+// });
 
-document.addEventListener("selectionchange", (e) => {
-  console.log("Archor node - ", window.getSelection().anchorNode);
-  console.log("Focus Node - ", window.getSelection().toString());
+// document.addEventListener("selectionchange", (e) => {
+//   console.log("Archor node - ", window.getSelection().anchorNode);
+//   console.log("Focus Node - ", window.getSelection().toString());
+// });
+
+document.addEventListener("mouseup", function (e) {
+  var txt = this.innerText;
+  var selection = window.getSelection();
+  var start = selection.anchorOffset;
+  var end = selection.focusOffset;
+  console.log(
+    "start at postion",
+    start,
+    "in node",
+    selection.anchorNode.wholeText
+  );
+  console.log(
+    "stop at position",
+    end,
+    "in node",
+    selection.focusNode.wholeText
+  );
 });
