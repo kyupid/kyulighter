6f906513e5871ad790f882e3c55dc8a06598ba20
kyupid <iamscvv@gmail.com>
2021-10-25 22:20:21 +0900

    태그로 간단히 하이라이트하기

--- main.js
+++ main.js
@@ -1,30 +1,47 @@
-// document.addEventListener("mouseup", (event) => {
-//   if (window.getSelection().toString().length) {
-//     let exactText = window.getSelection().toString();
-//     console.log(exactText);
-//   }
-// });
+document.addEventListener("mouseup", (event) => {
+  if (window.getSelection().toString().length) {
+    let selection = window.getSelection();
+    let exactText = selection.toString();
+
+    let span = document.createElement("SPAN");
+    span.textContent = exactText;
+
+    // ------getRangeAt() 역할 추론하기-----------
+    let ranges = [];
+    for (let i = 0; i < selection.rangeCount; i++) {
+      ranges[i] = selection.getRangeAt(i);
+      console.log(ranges);
+    }
+    // ---------------------------------------
+
+    let range = selection.getRangeAt(0);
+    range.deleteContents();
+    range.insertNode(span);
+  }
+});
 
 // document.addEventListener("selectionchange", (e) => {
 //   console.log("Archor node - ", window.getSelection().anchorNode);
 //   console.log("Focus Node - ", window.getSelection().toString());
 // });
 
-document.addEventListener("mouseup", function (e) {
-  var txt = this.innerText;
-  var selection = window.getSelection();
-  var start = selection.anchorOffset;
-  var end = selection.focusOffset;
-  console.log(
-    "start at postion",
-    start,
-    "in node",
-    selection.anchorNode.wholeText
-  );
-  console.log(
-    "stop at position",
-    end,
-    "in node",
-    selection.focusNode.wholeText
-  );
-});
+$(document).ready(function () {});
+
+// document.addEventListener("mouseup", function (e) {
+//   var txt = this.innerText;
+//   var selection = window.getSelection();
+//   var start = selection.anchorOffset;
+//   var end = selection.focusOffset;
+//   console.log(
+//     "start at postion",
+//     start
+//     // "in node",
+//     // selection.anchorNode.wholeText
+//   );
+//   console.log(
+//     "stop at position",
+//     end
+//     // "in node",
+//     // selection.focusNode.wholeText
+//   );
+// });
