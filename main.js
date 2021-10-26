da78e30864c8dcea7110cbff32f10e3675ceb240
kyupid <iamscvv@gmail.com>
2021-10-26 23:50:42 +0900

    선택한 문자열 부모 요소 이름과 인덱스 가져오기

--- main.js
+++ main.js
@@ -1,47 +1,34 @@
 document.addEventListener("mouseup", (event) => {
-  if (window.getSelection().toString().length) {
-    let selection = window.getSelection();
+  if (document.getSelection().toString().length) {
+    let selection = document.getSelection();
     let exactText = selection.toString();
 
     let span = document.createElement("SPAN");
     span.textContent = exactText;
 
-    // ------getRangeAt() 역할 추론하기-----------
-    let ranges = [];
-    for (let i = 0; i < selection.rangeCount; i++) {
-      ranges[i] = selection.getRangeAt(i);
-      console.log(ranges);
-    }
-    // ---------------------------------------
+    // // ------getRangeAt() 역할 추론하기-----------
+    // let ranges = [];
+    // for (let i = 0; i < selection.rangeCount; i++) {
+    //   ranges[i] = selection.getRangeAt(i);
+    //   console.log(ranges);
+    // }
+    // // ---------------------------------------
 
     let range = selection.getRangeAt(0);
     range.deleteContents();
     range.insertNode(span);
+
+    let parentElement = selection.anchorNode.parentElement;
+
+    let listByTagName = document.getElementsByTagName(a.tagName);
+
+    let indexOfTags = 0;
+    for (let i = 0; i < listByTagName.length; i++) {
+      if (parentElement === listByTagName[i]) {
+        indexOfTags = i;
+      }
+    }
+
+    console.log(indexOfTags);
   }
 });
-
-// document.addEventListener("selectionchange", (e) => {
-//   console.log("Archor node - ", window.getSelection().anchorNode);
-//   console.log("Focus Node - ", window.getSelection().toString());
-// });
-
-$(document).ready(function () {});
-
-// document.addEventListener("mouseup", function (e) {
-//   var txt = this.innerText;
-//   var selection = window.getSelection();
-//   var start = selection.anchorOffset;
-//   var end = selection.focusOffset;
-//   console.log(
-//     "start at postion",
-//     start
-//     // "in node",
-//     // selection.anchorNode.wholeText
-//   );
-//   console.log(
-//     "stop at position",
-//     end
-//     // "in node",
-//     // selection.focusNode.wholeText
-//   );
-// });
