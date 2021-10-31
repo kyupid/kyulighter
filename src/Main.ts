14a4a605b61dd4570901209c4cf61e79711ce20b
kyupid <iamscvv@gmail.com>
2021-10-31 16:44:05 +0900

    모듈 번들링 방식 변경

--- src/Main.ts
+++ src/Main.ts
@@ -1,81 +1,81 @@
-import {Meta} from "./Meta";
+import {Meta} from "./classes/Meta.js";
 
 if (window.localStorage.getItem("meta")) {
-  let metaData: Meta[];
-  const s = window.localStorage.getItem("meta") as string;
-  metaData = JSON.parse(s);
+    let metaData: Meta[];
+    const s = window.localStorage.getItem("meta") as string;
+    metaData = JSON.parse(s);
 
-  window.addEventListener("load", function () {
-    metaData.forEach((data) => {
-      let elements = document.getElementsByTagName(
-        data.tagName
-      ) as HTMLCollection;
-      let item = elements.item(data.indexOfTags) as Element;
+    window.addEventListener("load", function () {
+        metaData.forEach((data) => {
+            let elements = document.getElementsByTagName(
+                data.tagName
+            ) as HTMLCollection;
+            let item = elements.item(data.indexOfTags) as Element;
 
-      let innerHTML = item.innerHTML as string;
+            let innerHTML = item.innerHTML as string;
 
-      const index = innerHTML.indexOf(data.selectedText);
+            const index = innerHTML.indexOf(data.selectedText);
 
-      if (index >= 0) {
-        innerHTML =
-          innerHTML.substring(0, index) +
-          "<span>" +
-          innerHTML.substring(index, index + data.selectedText.length) +
-          "</span>" +
-          innerHTML.substring(index + data.selectedText.length);
-        item.innerHTML = innerHTML;
-      }
+            if (index >= 0) {
+                innerHTML =
+                    innerHTML.substring(0, index) +
+                    "<span>" +
+                    innerHTML.substring(index, index + data.selectedText.length) +
+                    "</span>" +
+                    innerHTML.substring(index + data.selectedText.length);
+                item.innerHTML = innerHTML;
+            }
+        });
     });
-  });
 }
 
 document.addEventListener("mouseup", () => {
-  if (document.getSelection()?.toString().length) {
-    const selection = document.getSelection() as Selection;
-    const selectedText = selection.toString();
+    if (document.getSelection()?.toString().length) {
+        const selection = document.getSelection() as Selection;
+        const selectedText = selection.toString();
 
-    const span = document.createElement("SPAN");
-    span.textContent = selectedText;
+        const span = document.createElement("SPAN");
+        span.textContent = selectedText;
 
-    const range = selection.getRangeAt(0);
-    range.deleteContents();
-    range.insertNode(span);
+        const range = selection.getRangeAt(0);
+        range.deleteContents();
+        range.insertNode(span);
 
-    const parentElement = selection.anchorNode?.parentElement as HTMLElement;
+        const parentElement = selection.anchorNode?.parentElement as HTMLElement;
 
-    const listByTagName = document.getElementsByTagName(parentElement.tagName);
+        const listByTagName = document.getElementsByTagName(parentElement.tagName);
 
-    let indexOfTags = 0;
-    for (let i = 0; i < listByTagName.length; i++) {
-      if (parentElement === listByTagName[i]) {
-        indexOfTags = i;
-      }
+        let indexOfTags = 0;
+        for (let i = 0; i < listByTagName.length; i++) {
+            if (parentElement === listByTagName[i]) {
+                indexOfTags = i;
+            }
+        }
+
+        const elements = document.getElementsByTagName(parentElement.tagName);
+        const textContent = elements.item(indexOfTags)?.textContent as string;
+        const startOffset = textContent.indexOf(selectedText) as number;
+
+        let selectedData = new Meta(
+            parentElement.tagName,
+            indexOfTags,
+            selectedText,
+            startOffset,
+            textContent
+        );
+
+        let metaData: Meta[];
+        if (window.localStorage.getItem("meta")) {
+            const s = window.localStorage.getItem("meta") as string;
+            metaData = JSON.parse(s);
+        } else {
+            metaData = [];
+        }
+
+        metaData.push(selectedData);
+        console.log(metaData);
+
+        const strLocationInfo = JSON.stringify(metaData);
+        window.localStorage.setItem("meta", strLocationInfo);
     }
-
-    const elements = document.getElementsByTagName(parentElement.tagName);
-    const textContent = elements.item(indexOfTags)?.textContent as string;
-    const startOffset = textContent.indexOf(selectedText) as number;
-
-    let selectedData = new Meta(
-      parentElement.tagName,
-      indexOfTags,
-      selectedText,
-      startOffset,
-      textContent
-    );
-
-    let metaData: Meta[];
-    if (window.localStorage.getItem("meta")) {
-      const s = window.localStorage.getItem("meta") as string;
-      metaData = JSON.parse(s);
-    } else {
-      metaData = [];
-    }
-
-    metaData.push(selectedData);
-    console.log(metaData);
-
-    const strLocationInfo = JSON.stringify(metaData);
-    window.localStorage.setItem("meta", strLocationInfo);
-  }
 });
